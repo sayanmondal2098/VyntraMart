9826fe2744ad77731c5f9ea957ff6ff847b228f3
saptakds <38220110+saptakds@users.noreply.github.com>
2020-04-25 10:54:52 +0530

    User (created) ● Product [created] ● Discount [created] ● Seller [created] ● Inventory [designed] ● Ratings [created]  ● Category [created]

---
+++ vmart/src/main/java/com/tmsl/vmart/model/Seller.java
@@ -0,0 +1,86 @@
+package com.tmsl.vmart.model;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="seller")
+public class Seller {
+
+	private Long sellerID;
+	private String name;
+	private String address;
+	private String phoneNo;
+	private String gpsCoordinates;
+	private Character verified;
+	private Set<Product> products;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getSellerID() {
+		return sellerID;
+	}
+	
+	public void setSellerID(Long sellerID) {
+		this.sellerID = sellerID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
+	public String getGpsCoordinates() {
+		return gpsCoordinates;
+	}
+	
+	public void setGpsCoordinates(String gpsCoordinates) {
+		this.gpsCoordinates = gpsCoordinates;
+	}
+	
+	public Character getVerified() {
+		return verified;
+	}
+	
+	public void setVerified(Character verified) {
+		this.verified = verified;
+	}
+	
+	@OneToMany(fetch = FetchType.LAZY,mappedBy = "seller")
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+	
+	
+	
+}
